d9938153c483a13877bb8ad566e736890ee928e6
Korobkin03 <maks.korobkin.03@mail.ru>
2023-03-26 13:12:44 +0300

    first section

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Html, useProgress } from "@react-three/drei";
 
-const CanvasLoader: React.FC = () => <section></section>;
+const CanvasLoader: React.FC = () => {
+  const { progress } = useProgress();
+
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#f1f1f1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
 
 export default CanvasLoader;
